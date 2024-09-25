8504fb659f42478807d3f32b0e9a2864d0d06da5
Tirinox <develoloper@tirinox.ru>
2024-09-25 09:41:54 +0300

    PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,47 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            manifest: {
+                name: 'Japanese Player',
+                short_name: 'JapPlayer',
+                description: 'A simple phrase player for learning Japanese',
+                icons: [
+                    {
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/logo.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ],
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                scope: '/',
+                start_url: '/',
+                orientation: 'portrait'
+            },
+            registerType: 'autoUpdate',
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
