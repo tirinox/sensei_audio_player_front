f1989b4f4a06c2e1978933e9df27456745dbc64d
Tirinox <tirinox@gmail.com>
2025-06-11 14:18:18 +0300

    Phrase index

--- src/components/KaraokeText.vue
+++ src/components/KaraokeText.vue
@@ -1,16 +1,28 @@
+<style>
+    rt {
+        color: #796b7e;
+    }
+    ruby {
+        color: black;
+    }
+</style>
 <template>
-  <div class="karaoke-text pb-2 mt-5">
-    <p class="phrase-text">
-      <span v-html="currentPhrase" v-if="showText"></span>
-      <span v-else>...</span>
-      <v-btn
-          :icon="showText ? 'mdi-eye-off' : 'mdi-eye'"
-          @click="showText = !showText"
-          density="comfortable"
-          variant="text"
-      ></v-btn>
-    </p>
-  </div>
+    <div class="karaoke-text pb-2">
+        <v-chip class="d">
+            {{ currentIndex }} / {{ phrases.length }}
+        </v-chip>
+
+        <p class="phrase-text mt-4 mb-2">
+            <span v-html="currentPhrase" v-if="showText"></span>
+            <span v-else>...</span>
+            <v-btn
+                :icon="showText ? 'mdi-eye-off' : 'mdi-eye'"
+                @click="showText = !showText"
+                density="comfortable"
+                variant="text"
+            ></v-btn>
+        </p>
+    </div>
 </template>
 
 <script setup>
@@ -20,18 +32,18 @@ import {parenthesesToRuby} from "@/helpers/Nihongo.js";
 const showText = ref(true);
 
 const props = defineProps({
-  phrases: {
-    type: Array,
-    required: true,
-  },
-  currentIndex: {
-    type: Number,
-    required: true,
-  },
+    phrases: {
+        type: Array,
+        required: true,
+    },
+    currentIndex: {
+        type: Number,
+        required: true,
+    },
 });
 
 const currentPhrase = computed(() => {
-  return parenthesesToRuby(props.phrases[props.currentIndex - 1].text)
+    return parenthesesToRuby(props.phrases[props.currentIndex - 1].text)
 });
 
 // const previousPhrase = computed(() => {
@@ -52,13 +64,13 @@ const currentPhrase = computed(() => {
 <style scoped>
 
 .phrase-text {
-  font-size: 2em;
+    font-size: 2em;
 }
 
 .blurry {
-  font-size: 1.5em;
-  color: #ccc;
-  text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+    font-size: 1.5em;
+    color: #ccc;
+    text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
 }
 
 </style>
